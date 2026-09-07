675b3aaaa079581fd5310095081d4a7ccc8b228e
zakariamenas5-sketch <zakariamenas5@gmail.com>
2026-09-07 23:11:25 +0100

    Add MaktabtiApp entry point

---
+++ Sources/MaktabtiApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MaktabtiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
